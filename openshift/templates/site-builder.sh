414d5cf1f54277ed5032f05cfe9c969a0d0e6400
Struk <Alex.Struk@gov.bc.ca>
2023-01-23 11:33:34 -0800

    fork changes
    - added digital.gov.bc.ca specific namespaces
    - rolled back some changes related to configuration files to be included in config yaml files
    - changed addresses to point to artifactory instead of docker-remote

--- openshift/templates/site-builder.sh
+++ openshift/templates/site-builder.sh
@@ -13,36 +13,36 @@ if [ ! -z "${OC_ENV}" ] && [ ! -z "${OC_APP_DOMAIN}" ] && [ ! -z "${OC_SITE_NAME
         templates="$(dirname $0)";
         printf >&2 "\nStarting Build......with user ${whoAmI}\n"
 
-        printf >&2 "\nCONFIG SETUP\n"
+        # printf >&2 "\nCONFIG SETUP\n"
         oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p APP_DOMAIN=${OC_APP_DOMAIN} -p WORDPRESS_MULTISITE=0 -f ${templates}/config/config.yaml | oc apply --overwrite=false -f -
         
-        printf >&2 "\nMARIADB SETUP\n"
-        echo "Setting up secrets"
-        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -f ${templates}/mariadb/secrets.yaml | oc apply --overwrite=false -f -
-        echo "Setting up volume"
-        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -f ${templates}/mariadb/volume.yaml | oc apply --overwrite=false -f -
-        echo "Deploying"
-        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p POOL_NAME=${OC_POOL_NAME} -f ${templates}/mariadb/deploy.yaml | oc apply -f -
+        # printf >&2 "\nMARIADB SETUP\n"
+        # echo "Setting up secrets"
+        # oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -f ${templates}/mariadb/secrets.yaml | oc apply --overwrite=false -f -
+        # echo "Setting up volume"
+        # oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -f ${templates}/mariadb/volume.yaml | oc apply --overwrite=false -f -
+        # echo "Deploying"
+        # oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p POOL_NAME=${OC_POOL_NAME} -f ${templates}/mariadb/deploy.yaml | oc apply -f -
 
-        printf >&2 "\nNGINX SETUP\n"
+        # printf >&2 "\nNGINX SETUP\n"
 
-        echo "Setting up service and routes"
-        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p APP_DOMAIN=${OC_APP_DOMAIN} -f ${templates}/nginx/deploy-route.yaml | oc apply --overwrite=false -f -
+        # echo "Setting up service and routes"
+        # oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p APP_DOMAIN=${OC_APP_DOMAIN} -f ${templates}/nginx/deploy-route.yaml | oc apply --overwrite=false -f -
 
-        printf >&2 "\nWORDPRESS SETUP\n"
-        echo "Setting up secrets"
-        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -f ${templates}/wordpress/secrets.yaml | oc apply --overwrite=false -f -
-        echo "Setting up volume"
-        oc process -p ENV_NAME=${OC_ENV} -p POOL_NAME=${OC_POOL_NAME} -f ${templates}/wordpress/volume.yaml | oc apply -f -
-        echo "Deploying"
-        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p POOL_NAME=${OC_POOL_NAME} -f ${templates}/wordpress/deploy-nginx-wordpress.yaml | oc apply -f -
+        # printf >&2 "\nWORDPRESS SETUP\n"
+        # echo "Setting up secrets"
+        # oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -f ${templates}/wordpress/secrets.yaml | oc apply --overwrite=false -f -
+        # echo "Setting up volume"
+        # oc process -p ENV_NAME=${OC_ENV} -p POOL_NAME=${OC_POOL_NAME} -f ${templates}/wordpress/volume.yaml | oc apply -f -
+        # echo "Deploying"
+        # oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p POOL_NAME=${OC_POOL_NAME} -f ${templates}/wordpress/deploy-nginx-wordpress.yaml | oc apply -f -
 
-        printf >&2 "\nBACKUPS SETUP\n"
-        echo "Setting up volumes"
-        oc process -p ENV_NAME=${OC_ENV} -p POOL_NAME=${OC_POOL_NAME} -f ${templates}/backups/volume-backup.yaml | oc apply -f -
-        oc process -p ENV_NAME=${OC_ENV} -p POOL_NAME=${OC_POOL_NAME} -f ${templates}/backups/volume-verification.yaml | oc apply -f -
-        echo "Setting up cron conainer"
-        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p POOL_NAME=${OC_POOL_NAME} -f ${templates}/backups/cron.yaml | oc apply -f -
+        # printf >&2 "\nBACKUPS SETUP\n"
+        # echo "Setting up volumes"
+        # oc process -p ENV_NAME=${OC_ENV} -p POOL_NAME=${OC_POOL_NAME} -f ${templates}/backups/volume-backup.yaml | oc apply -f -
+        # oc process -p ENV_NAME=${OC_ENV} -p POOL_NAME=${OC_POOL_NAME} -f ${templates}/backups/volume-verification.yaml | oc apply -f -
+        # echo "Setting up cron conainer"
+        # oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p POOL_NAME=${OC_POOL_NAME} -f ${templates}/backups/cron.yaml | oc apply -f -
     else
         printf >&2 "$(oc whoami)"
     fi
