5648313b379aaf15bb75706bee1b0560c6b6d5df
kmandryk <Kaegan.Mandryk@gov.bc.ca>
2023-04-21 10:12:11 -0700

    Update site-builder-unix.sh

--- openshift/templates/site-builder-unix.sh
+++ openshift/templates/site-builder-unix.sh
@@ -14,24 +14,24 @@ if [ ! -z "${OC_ENV}" ] && [ ! -z "${OC_APP_DOMAIN}" ] && [ ! -z "${OC_SITE_NAME
         printf >&2 "\nStarting Build......with user ${whoAmI}\n"
 
         # printf >&2 "\nCONFIG SETUP\n"
-        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p APP_DOMAIN=${OC_APP_DOMAIN} -p WORDPRESS_MULTISITE=0 -p APP_NAME=${OC_SITE_NAME} -f ${templates}/config/config.yaml | oc apply --overwrite=false -f -
+        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p APP_DOMAIN=${OC_APP_DOMAIN} -p WORDPRESS_MULTISITE=0 -p APP_NAME=${OC_SITE_NAME} -f ${templates}/config/config.yaml | oc apply -f -
         
         printf >&2 "\nMARIADB SETUP\n"
         echo "Setting up secrets"
-        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p APP_NAME=${OC_SITE_NAME} -p MYSQL_ROOT_PASSWORD=${ROOT_PASSWORD} -f ${templates}/mariadb/secrets.yaml | oc apply --overwrite=false -f -
+        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p APP_NAME=${OC_SITE_NAME} -p MYSQL_ROOT_PASSWORD=${ROOT_PASSWORD} -f ${templates}/mariadb/secrets.yaml | oc apply -f -
         echo "Setting up volume"
-        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p APP_NAME=${OC_SITE_NAME} -f ${templates}/mariadb/volume.yaml | oc apply --overwrite=false -f -
+        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p APP_NAME=${OC_SITE_NAME} -f ${templates}/mariadb/volume.yaml | oc apply -f -
         echo "Deploying"
         oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p POOL_NAME=${OC_POOL_NAME} -p APP_NAME=${OC_SITE_NAME} -f ${templates}/mariadb/deploy.yaml | oc apply -f -
 
         printf >&2 "\nNGINX SETUP\n"
 
         echo "Setting up service and routes"
-        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p APP_DOMAIN=${OC_APP_DOMAIN} -p APP_NAME=${OC_SITE_NAME} -f ${templates}/nginx/deploy-route.yaml | oc apply --overwrite=false -f -
+        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p APP_DOMAIN=${OC_APP_DOMAIN} -p APP_NAME=${OC_SITE_NAME} -f ${templates}/nginx/deploy-route.yaml | oc apply -f -
 
         printf >&2 "\nWORDPRESS SETUP\n"
         echo "Setting up secrets"
-        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p APP_NAME=${OC_SITE_NAME} -f ${templates}/wordpress/secrets.yaml | oc apply --overwrite=false -f -
+        oc process -p ENV_NAME=${OC_ENV} -p SITE_NAME=${OC_SITE_NAME} -p APP_NAME=${OC_SITE_NAME} -f ${templates}/wordpress/secrets.yaml | oc apply -f -
         echo "Setting up volume"
         oc process -p ENV_NAME=${OC_ENV} -p POOL_NAME=${OC_POOL_NAME} -p APP_NAME=${OC_SITE_NAME} -f ${templates}/wordpress/volume.yaml | oc apply -f -
         echo "Deploying"
